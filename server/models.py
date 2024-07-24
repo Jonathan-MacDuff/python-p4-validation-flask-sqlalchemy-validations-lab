e22894c67f251de9d156e61fac10890be26b0372
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-07-24 14:27:33 -0700

    completed lab

--- server/models.py
+++ server/models.py
@@ -11,7 +11,23 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name', 'phone_number')
+    def validate_author(self, key, data):
+        if key == 'name':
+            if len(data) == 0:
+                raise ValueError("Name must not be blank")
+            else:
+                for n in Author.query.all():
+                    if n.name == data:
+                        raise ValueError("Name must be unique")
+        elif key == 'phone_number':
+            if len(data) != 10:
+                raise ValueError("Phone number must be 10 digits")
+            else:
+                for char in data:
+                    if not char.isdigit():
+                        raise ValueError("Phone number must not include any non-integer characters")
+        return data
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +43,21 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates('title', 'content', 'category', 'summary')
+    def validate_post(self, key, data):
+        if key == 'title':
+            if not ("Won't Believe" in data or "Secret" in data or "Top" in data or "Guess" in data):
+                raise ValueError("Title must conatain clickbait")
+        elif key == 'content':
+            if len(data) < 250:
+                raise ValueError("Content must be at least 250 characters")
+        elif key == 'category':
+            if not (data == "Fiction" or data == "Non-Fiction"):
+                raise ValueError("Category must be Fiction or Non-Fiction")
+        elif key == 'summary':
+            if len(data) > 250:
+                raise ValueError("Summary must be no more than 250 characters")
+        return data
 
 
     def __repr__(self):
